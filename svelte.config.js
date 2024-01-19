e289b510512e3ab205032d4db13da501b974c29f
NRDsstuff <nrdfoss@gmail.com>
2024-01-19 18:32:49 +0100

    Reapply "init implementation of static site"
    This reverts commit 70f99d264d96890306c738cddb4fddf92ef54d8b.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// we'll add github pages soon cya <33333333333333333
-// I have to go :3 <3
-// ilysm my boyfriend :3 <3 ilysm my girlfriend <3333333333333333
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,10 +10,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: "404.html"
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 		alias: {
 			"$kit": "./src/lib/kit/"
 		}
